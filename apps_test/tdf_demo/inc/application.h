45113feb811e8dad8fba7e46cb0ec2df304e72c3
Jordan Yates <jordan.yates@data61.csiro.au>
2020-04-23 21:43:24 +1000

    apps_test: tdf_demo: Adds TDF demonstration
    Adds an example application that switches between various TDFs
    with different timestamping.
    
    Also adds a demo python script that prints packets distributed by
    baselisten.

---
+++ apps_test/tdf_demo/inc/application.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020, Commonwealth Scientific and Industrial Research
+ * Organisation (CSIRO) ABN 41 687 119 230.
+ *
+ * Filename: application.h
+ * Creation_Date: 23/04/2020
+ * Author: Jordan Yates <jordan.yates@data61.csiro.au>
+ *
+ * Application specific configuration
+ *
+ */
+#ifndef __CSIRO_CORE_APPLICATION
+#define __CSIRO_CORE_APPLICATION
+/* Includes -------------------------------------------------*/
+
+/* Module Defines -------------------------------------------*/
+// clang-format off
+
+#define APP_MAJOR                               255
+#define APP_MINOR                               255
+
+#define NVM_VALID_KEY                           0x12345678
+
+// clang-format on
+/* Type Definitions -----------------------------------------*/
+
+#endif /* __CSIRO_CORE_APPLICATION */
